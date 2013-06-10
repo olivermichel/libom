13600682fd2c57b6281584acc6b85ab15d99333f
Oliver Michel <oliver.michel@editum.de>
2013-06-09 22:45:49 -0600

    remove most function templates from Socket

--- include/om/net/socket.h
+++ include/om/net/socket.h
@@ -1,53 +1,32 @@
 //
 //  Olli's C++ Library
 //  net/socket.h
-//  (c) 2012 Oliver Michel
+//  (c) 2013 Oliver Michel <oliver dot michel at editum dot de>
+//  
+//  This class introduces an abstraction level for socket-like I/O devices.
+//  To be extended with functions like packet/byte counters at some point.
 //
 
 #ifndef OM_NET_SOCKET_H
 #define OM_NET_SOCKET_H
 
-#include <stdexcept>
-#include <iostream>
-#include <cstring>
-
-#include <sys/select.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-
 #include <om/net/net.h>
 #include <om/net/io_interface.h>
 
 namespace om {
-  namespace net {
+	namespace net {
 
-    class Socket : public om::net::IOInterface {
+		class Socket : public om::net::IOInterface {
 
-    public:
+		public:
 
-      explicit Socket(const om::net::IOInterface::iface_type type,
-        const om::net::tp_addr addr);
-      
-      Socket(const om::net::Socket &copy_from);
-      
-      Socket& operator=(Socket& copy_from);
+			explicit Socket(const om::net::IOInterface::iface_type typ);
+			Socket(const om::net::Socket &copy_from);  
+			Socket& operator=(Socket& copy_from);
 
-      om::net::tp_addr addr();
-
-      virtual int open(const om::net::tp_addr addr) = 0;
-
-      virtual int send(const om::net::tp_addr remote_addr, 
-        const unsigned char *tx_data, const size_t data_len) = 0;
-
-      virtual int receive(om::net::tp_addr *from, unsigned char *rx_buffer, 
-        const size_t buffer_len) = 0;
-
-      virtual ~Socket() = 0;
-
-    protected:
-      om::net::tp_addr _addr;
-    };
-  }
+			virtual ~Socket();
+		};
+	}
 }
 
 #endif
--- src/net/socket.cc
+++ src/net/socket.cc
@@ -1,29 +1,21 @@
 //
 //  Olli's C++ Library
 //  net/socket.cc
-//  (c) 2012 Oliver Michel
+//  (c) 2013 Oliver Michel <oliver dot michel at editum dot de>
 //
 
 #include <om/net/socket.h>
 
-om::net::Socket::Socket(const om::net::IOInterface::iface_type type,
-  const om::net::tp_addr addr)
-  : om::net::IOInterface(type), _addr(addr) {} 
+om::net::Socket::Socket(const om::net::IOInterface::iface_type type)
+	: om::net::IOInterface(type) {}
 
 om::net::Socket::Socket(const om::net::Socket &copy_from)
-  : om::net::IOInterface(copy_from), _addr(copy_from._addr) {} 
+	: om::net::IOInterface(copy_from) {}
 
 om::net::Socket& om::net::Socket::operator=(om::net::Socket& copy_from) {
 
-  om::net::IOInterface::operator=(copy_from);
-  _addr = copy_from._addr;
-
-  return *this;
-}
-
-om::net::tp_addr om::net::Socket::addr() {
-
-  return _addr;
+	om::net::IOInterface::operator=(copy_from);
+	return *this;
 }
 
 om::net::Socket::~Socket() {}
