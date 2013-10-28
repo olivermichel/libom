9e93fc62ba7d7f3b7c70f480eb3ba24b905192d0
Matthew Monaco <matthew.monaco@0x01b.net>
2013-10-28 17:15:12 -0600

    Never touch another man's whitespace!

--- include/om/tools/file.h
+++ include/om/tools/file.h
@@ -21,11 +21,11 @@ namespace om {
 			template <class T>
 			T read_uint_lit(std::string file_name) throw(std::runtime_error);
 
-      template <class T>
-      void write_int_lit(std::string file_name, T data) throw(std::runtime_error);
+			template <class T>
+			void write_int_lit(std::string file_name, T data) throw(std::runtime_error);
 
-      template <class T>
-      T read_int_lit(std::string file_name) throw(std::runtime_error);
+			template <class T>
+			T read_int_lit(std::string file_name) throw(std::runtime_error);
 
 		}
 	}
