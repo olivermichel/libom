0f70edc9a5efd116b89f5752385c74d8f80208ac
Oliver Michel <oliver.michel@editum.de>
2013-11-25 21:35:46 -0700

    clean up IOInterface

--- om/net/io_interface.cc
+++ om/net/io_interface.cc
@@ -1,50 +1,51 @@
 //
-//  Olli's C++ Library
+//  Olli's C++ Library [https://bitbucket.org/omichel/om-lib]
 //  net/io_interface.cc
-//  (c) 2012 Oliver Michel
+//  (c) 2012 Oliver Michel <oliver dot michel at editum dot de>
+//  http://ngn.cs.colorado/~oliver
 //
 
 #include "io_interface.h"
 
 om::net::IOInterface::IOInterface(om::net::IOInterface::iface_type type) 
-  : _fd(0), _iface_type(type) {}
+	: _fd(0), _iface_type(type) {}
 
 om::net::IOInterface::IOInterface(const om::net::IOInterface &copy_from) 
-  : _fd(copy_from._fd), _iface_type(copy_from._iface_type) {}
+	: _fd(copy_from._fd), _iface_type(copy_from._iface_type) {}
 
 om::net::IOInterface& 
-  om::net::IOInterface::operator=(const om::net::IOInterface& copy_from) {
+	om::net::IOInterface::operator=(const om::net::IOInterface& copy_from) {
 
-  _fd = copy_from._fd;
-  _iface_type = copy_from._iface_type;
+	_fd = copy_from._fd;
+	_iface_type = copy_from._iface_type;
 
-  return *this;
+	return *this;
 }
 
 int om::net::IOInterface::fd() {
 
-  return _fd;
+	return _fd;
 }
 
 void om::net::IOInterface::add_to_fd_set(fd_set *fds)
-  throw(std::logic_error) {
+	throw(std::logic_error) {
 
-  if(_fd == 0)
-    throw std::logic_error("add_to_fd_set(): Interface is not opened");
+	if(_fd == 0)
+		throw std::logic_error("add_to_fd_set(): Interface is not opened");
 
-  FD_SET(_fd, fds);
+	FD_SET(_fd, fds);
 }
 
 void om::net::IOInterface::remove_from_fd_set(fd_set *fds)
-  throw(std::logic_error) {
+	throw(std::logic_error) {
 
-  if(_fd == 0)
-    throw std::logic_error("remove_from_fd_set: Interface is not opened");
-  
-  FD_CLR(_fd, fds);
+	if(_fd == 0)
+		throw std::logic_error("remove_from_fd_set: Interface is not opened");
+	
+	FD_CLR(_fd, fds);
 }
 
 om::net::IOInterface::iface_type om::net::IOInterface::type() {
 
-  return _iface_type;
+	return _iface_type;
 }
--- om/net/io_interface.h
+++ om/net/io_interface.h
@@ -1,55 +1,57 @@
 //
-//  Olli's C++ Library
+//  Olli's C++ Library [https://bitbucket.org/omichel/om-lib]
 //  net/io_interface.h
-//  (c) 2012 Oliver Michel
+//  (c) 2012 Oliver Michel <oliver dot michel at editum dot de>
+//  http://ngn.cs.colorado/~oliver
+//
+//  Provides an abstraction for I/O interfaces compatible with om::net::Agent
 //
 
 #ifndef OM_NET_IO_INTERFACE_H
 #define OM_NET_IO_INTERFACE_H
 
 #include <stdexcept>
-
 #include <sys/select.h>
 
-#include "net.h"
+#include <om/net/net.h>
 
 namespace om {
-  namespace net {
+	namespace net {
 
-    class IOInterface {
+		class IOInterface {
 
-    public:
+		public:
 
-      enum iface_type {
-        iface_type_unknown,
-        iface_type_sock_raw,
-        iface_type_sock_stream,
-        iface_type_sock_dgram,
-        iface_type_tun,
-        iface_type_inotify_handler
-      };
+			enum iface_type {
+				iface_type_unknown,
+				iface_type_sock_raw,
+				iface_type_sock_stream,
+				iface_type_sock_dgram,
+				iface_type_tun,
+				iface_type_inotify_handler
+			};
 
-      explicit IOInterface(iface_type type);
-      IOInterface(const IOInterface& copy_from);
-      IOInterface& operator=(const IOInterface& copy_from);
+			explicit IOInterface(iface_type type);
+			IOInterface(const IOInterface& copy_from);
+			IOInterface& operator=(const IOInterface& copy_from);
 
-      int fd();
+			int fd();
 
-      void add_to_fd_set(fd_set *fds) throw(std::logic_error);
-      void remove_from_fd_set(fd_set *fds) throw(std::logic_error);
+			void add_to_fd_set(fd_set *fds) throw(std::logic_error);
+			void remove_from_fd_set(fd_set *fds) throw(std::logic_error);
 
-      iface_type type();
+			iface_type type();
 
-      virtual void close() {}
-      virtual ~IOInterface() {}
-    
-    protected:
-      int _fd;
+			virtual void close() {}
+			virtual ~IOInterface() {}
+		
+		protected:
+			int _fd;
 
-    private:
-      iface_type _iface_type;
-    };
-  }
+		private:
+			iface_type _iface_type;
+		};
+	}
 }
 
 #endif
