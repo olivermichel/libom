8149f6a74b29ab05caacbf0eec51d441b9bc2ff7
Oliver Michel <oliver.michel@editum.de>
2014-01-29 06:07:42 -0700

    om::tools::file::copy_all_files()

--- om/tools/file.cc
+++ om/tools/file.cc
@@ -9,12 +9,14 @@
 #include <stdint.h>
 #include <fstream>
 
+#include <om/tools/dir.h>
 
 #include "file.h"
 
 template<class T>
 void om::tools::file::write_uint_lit(std::string file_name, T data)
-	throw(std::runtime_error) {
+	throw(std::runtime_error)
+{
 
 	std::ofstream file(file_name.c_str(), std::ios::out | std::ios::trunc);
 
@@ -28,7 +30,8 @@ void om::tools::file::write_uint_lit(std::string file_name, T data)
 
 template<class T>
 T om::tools::file::read_uint_lit(std::string file_name)
-	throw(std::runtime_error) {
+	throw(std::runtime_error)
+{
 
 	T value = 0;
 	std::ifstream file(file_name.c_str(), std::ios::in);
@@ -46,7 +49,8 @@ T om::tools::file::read_uint_lit(std::string file_name)
 
 template<class T>
 void om::tools::file::write_int_lit(std::string file_name, T data)
-	throw(std::runtime_error) {
+	throw(std::runtime_error)
+{
 
 	std::ofstream file(file_name.c_str(), std::ios::out | std::ios::trunc);
 
@@ -60,7 +64,8 @@ void om::tools::file::write_int_lit(std::string file_name, T data)
 
 template<class T>
 T om::tools::file::read_int_lit(std::string file_name)
-	throw(std::runtime_error) {
+	throw(std::runtime_error)
+{
 
 	T value = 0;
 	std::ifstream file(file_name.c_str(), std::ios::in);
@@ -74,6 +79,56 @@ T om::tools::file::read_int_lit(std::string file_name)
 	file.close();
 
 	return value;
+}
+
+void om::tools::file::write_data(std::string file_name, const char* buf, size_t len)
+	throw(std::runtime_error)
+{
+	std::ofstream file(file_name.c_str(), std::ios::out | std::ios::trunc | std::ios::binary);
+
+	if(file.good()) {
+
+		file.write(buf, len);
+		file.close();
+
+	} else throw std::runtime_error("write_data: failed openining file");
+}
+
+void om::tools::file::copy(std::string from, std::string to)
+	throw(std::runtime_error)
+{
+
+	std::ifstream from_file(from);
+	std::ofstream to_file(to);
+
+	if(from_file.is_open() && to_file.is_open()) {
+
+		while(from_file.good())
+			to_file.put(from_file.get());
+
+		from_file.close(), to_file.close();
+
+	} else throw std::runtime_error("copy: failed openining file");
+}
+
+void om::tools::file::copy_all_files(std::string from, std::string to)
+	throw(std::runtime_error)
+{
+	for(om::tools::dir::entry e : om::tools::dir::contents(from))
+		if(e.type == om::tools::dir::file_type_reg)
+			om::tools::file::copy(from + "/" + e.name, to + "/" + e.name);
+}
+
+void om::tools::file::touch(std::string file)
+	throw(std::runtime_error)
+{
+
+	std::ofstream fs(file);
+
+	if(!fs.is_open())
+		throw std::runtime_error("touch: failed openining file");
+
+	fs.close();
 }
 
 template void om::tools::file::write_uint_lit<uint8_t>(std::string file_name, 
@@ -117,48 +172,3 @@ template void om::tools::file::write_int_lit<int64_t>(std::string file_name,
 	int64_t data);
 
 template int64_t om::tools::file::read_int_lit<int64_t>(std::string file_name);
-
-
-void om::tools::file::write_data(std::string file_name, const char* buf, size_t len)
-	throw(std::runtime_error) {
-
-	std::ofstream file(file_name.c_str(), std::ios::out | std::ios::trunc | std::ios::binary);
-
-	if(file.good()) {
-
-		file.write(buf, len);
-		file.close();
-
-	} else throw std::runtime_error("write_data: failed openining file");
-}
-
-void om::tools::file::copy(std::string from, std::string to)
-	throw(std::runtime_error) {
-
-	std::ifstream from_file(from);
-	std::ofstream to_file(to);
-
-	if(from_file.is_open() && to_file.is_open()) {
-
-		while(from_file.good())
-			to_file.put(from_file.get());
-
-		from_file.close(), to_file.close();
-
-	} else throw std::runtime_error("copy: failed openining file");
-}
-
-
-void om::tools::file::touch(std::string file)
-	throw(std::runtime_error) {
-
-	std::ofstream fs(file);
-
-	if(!fs.is_open())
-		throw std::runtime_error("touch: failed openining file");
-
-	fs.close();
-}
-
-
-
--- om/tools/file.h
+++ om/tools/file.h
@@ -18,23 +18,31 @@ namespace om {
 		namespace file {
 
 			template <class T>
-			void write_uint_lit(std::string file_name, T data) throw(std::runtime_error);
+			void write_uint_lit(std::string file_name, T data) 
+				throw(std::runtime_error);
 
 			template <class T>
-			T read_uint_lit(std::string file_name) throw(std::runtime_error);
+			T read_uint_lit(std::string file_name) 
+				throw(std::runtime_error);
 
 			template <class T>
-			void write_int_lit(std::string file_name, T data) throw(std::runtime_error);
+			void write_int_lit(std::string file_name, T data) 
+				throw(std::runtime_error);
 
 			template <class T>
 			T read_int_lit(std::string file_name) throw(std::runtime_error);
 
-			void write_data(std::string file_name, const char* buf, size_t len) throw(std::runtime_error);
+			void write_data(std::string file_name, const char* buf, size_t len) 
+				throw(std::runtime_error);
 
-			void copy(std::string from, std::string to) throw(std::runtime_error);
+			void copy(std::string from, std::string to)
+				throw(std::runtime_error);
 
-			void touch(std::string file) throw(std::runtime_error);
+			void copy_all_files(std::string from, std::string to)
+				throw(std::runtime_error);
 
+			void touch(std::string file)
+				throw(std::runtime_error);
 		}
 	}
 }
