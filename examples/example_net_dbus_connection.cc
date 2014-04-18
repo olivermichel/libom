b38e3142c256f4c380a2f0ce355d0fd2febf399a
Oliver Michel <oliver.michel@editum.de>
2014-04-17 21:19:24 -0600

    empty example for DBusConnection

---
+++ examples/example_net_dbus_connection.cc
@@ -0,0 +1,18 @@
+//
+//  Olli's C++ Library [https://bitbucket.org/omichel/om-lib]
+//  examples/example_net_dbus_connetion.cc
+//  (c) 2014 Oliver Michel <oliver dot michel at editum dot de>
+//  http://ngn.cs.colorado/~oliver
+//
+
+#include <iostream>
+#include <unistd.h>
+
+#include <om/net/dbus_connection.h>
+
+int main(int argc, char const *argv[]) {
+
+
+
+	return 0;
+}
