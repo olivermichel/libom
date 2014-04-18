d5581ed260ac65114c31fcceebb1a433daddc368
Oliver Michel <oliver.michel@editum.de>
2014-04-18 14:11:00 -0600

    DBusAdapter: integrate with Agent

--- examples/example_net_dbus_adapter.cc
+++ examples/example_net_dbus_adapter.cc
@@ -8,40 +8,49 @@
 #include <iostream>
 #include <unistd.h>
 
+#include <om/net/agent.h>
 #include <om/net/dbus_adapter.h>
+
+class ExampleDBusAdapterAgent : public om::net::Agent {
+
+public:
+	
+	void connected(om::net::DBusAdapter* dbus) {
+
+		std::cout << "connected: " << dbus->unique_name() << std::endl;
+		std::cout << "fd: " << dbus->fd() << std::endl;
+
+		om::net::DBusSignal sig(
+			"/test/signal/Object",
+			"test.signal.Type",
+			"Test"
+		);
+
+		dbus->match_signal("test.signal.Type");
+		
+		dbus->send_signal(sig);
+	}
+
+};
 
 int main(int argc, char const *argv[]) {
 
-	om::net::DBusAdapter dbus;
+	ExampleDBusAdapterAgent a;
+
+	om::net::DBusAdapter* dbus = new om::net::DBusAdapter();
 
 	try {
-
-		dbus.connect("unix:path=/tmp/test_bus", "edu.colorado.cs.ngn.DBusTest",
-			[](om::net::DBusAdapter* dbus) {
-
-				std::cout << "connected: " << dbus->unique_name() << std::endl;
-				std::cout << "fd: " << dbus->fd() << std::endl;
-
-				om::net::DBusSignal sig(
-					"/test/signal/Object",
-					"test.signal.Type",
-					"Test"
-				);
-
-				dbus->match_signal("test.signal.Type");
-				
-				dbus->send_signal(sig);
-
+		dbus->connect("unix:path=/tmp/test_bus", "edu.colorado.cs.ngn.DBusTest",
+			[&a](om::net::DBusAdapter* dbus) {
+				a.add_interface(dbus), a.connected(dbus);
+				a.run();
 			}
 		);
-	
 
 	} catch(std::exception& e) {
 		std::cout << e.what() << std::endl;
 		std::exit(EXIT_FAILURE);
 	}
-
-
 
 	return 0;
 }
